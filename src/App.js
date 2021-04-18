eddd4b8f7964eff52eafbbacb578bdf04fcdd631
Vasilok3333 <vasylshchur3333@gmail.com>
2021-04-18 17:32:05 +0300

    cart component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json";
@@ -8,47 +9,80 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       price: "",
       sort: "",
       company: "",
     };
     this.sortProducts = this.sortProducts.bind(this);
     this.filterCompanyProducts = this.filterCompanyProducts.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+    this.removeFromCart = this.removeFromCart.bind(this);
   }
 
   sortProducts(event) {
-const sort = event.target.value;
+    const sort = event.target.value;
 
-    this.setState( (state) => ({
-      sort:sort,
-      products: [...this.state.products].sort((a,b) => 
-      sort === "lowest" ? 
-      (a.price - b.price) : 
-      sort === "highest" ? 
-      (b.price - a.price) :
-      (b.id - a.id )
-      ) 
-    })
-    )
-    console.log(event.target.value);
+    this.setState((state) => ({
+      sort: sort,
+      products: [...this.state.products].sort((a, b) =>
+        sort === "lowest"
+          ? a.price - b.price
+          : sort === "highest"
+          ? b.price - a.price
+          : b.id - a.id
+      ),
+    }));
   }
 
   filterCompanyProducts(event) {
     console.log(event.target.value);
-if (event.target.value === "all") {
-  this.setState({
-    company: event.target.value,
-    products: data.products
-  })
-} else {
+    if (event.target.value === "all") {
+      this.setState({
+        company: event.target.value,
+        products: data.products,
+      });
+    } else {
+      this.setState({
+        company: event.target.value,
+        products: data.products.filter(
+          (product) =>
+            product.company
+              .toUpperCase()
+              .indexOf(event.target.value.toUpperCase()) >= 0
+        ),
+      });
+    }
+  }
+
+  addToCart(product) {
+    console.log(product);
+
+    let cartItems = this.state.cartItems.slice();
+    console.log(cartItems);
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item.id === product.id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+
     this.setState({
-      company: event.target.value,
-      products: data.products.filter(
-        (product) => product.company.toUpperCase().indexOf(event.target.value.toUpperCase()) >= 0
-      ),
+      cartItems,
     });
   }
-}
+
+  removeFromCart(product) {
+    let cartItems = this.state.cartItems.slice();
+   
+    this.setState({
+      cartItems: cartItems.filter((item) => item.id !== product.id),
+    })
+  }
 
   render() {
     return (
@@ -66,9 +100,17 @@ if (event.target.value === "all") {
                 sortProducts={this.sortProducts}
                 filterCompanyProducts={this.filterCompanyProducts}
               />
-              <Products products={this.state.products} />
+              <Products
+                addToCart={this.addToCart}
+                products={this.state.products}
+              />
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
           </div>
         </main>
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -5,25 +5,30 @@ import formatCurrency from "../util";
 export default class Products extends Component {
   render() {
     return (
+      
       <div>
-        <ul className={s.products}>
-          {this.props.products.map((product) => (
-            <li key={product.id}>
-              <div className={s.product}>
-                <a href={"#" + product.id}>
-                  <img src={product.img} alt={product.title}></img>
-                  <p>{product.title}</p>
-                </a>
-                <div className={s.productPrice}>
-                  <div>{formatCurrency(product.price)}</div>
-                  <button className={`${s.button} ${s.primary}`}>
-                    Add to card
+        { this.props.products.length > 0 ?
+          <ul className={s.products}>
+            {this.props.products.map((product) => (
+              <li key={product.id}>
+                <div className={s.product}>
+                  <a href={"#" + product.id}>
+                    <img src={product.img} alt={product.title}></img>
+                    <p>{product.title}</p>
+                  </a>
+                  <div className={s.productPrice}>
+                    <div>{formatCurrency(product.price)}</div>
+                    <button onClick={() => this.props.addToCart(product)} className={`${s.button} ${s.primary}`}>
+                      Add to card
                   </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul> :
+          <div className={s.emptyList}>За заданими параметрами нічого не знайдено. Попробуйте змінити параметри фільтру</div>
+        
+        }
       </div>
     );
   }
