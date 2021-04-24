886e90ba8aab2d47d40e6eaa57be493d5e2e2df1
Vasilok3333 <vasylshchur3333@gmail.com>
2021-04-24 23:17:50 +0300

    added back end

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/my-online-shop-db", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const Product = mongoose.model("products", new mongoose.Schema({
+    id: { type: String, default: shortid.generate },
+    title: String,
+    img: String,
+    price: Number,
+    company: String,
+    color: [String],
+    info: String,
+}));
+
+app.get("/api/products", async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+});
+
+app.post("/api/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    const saveProduct = await newProduct.save();
+    res.send(saveProduct);
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.send(deletedProduct);
+})
+
+const port = process.env.PORT || 5006;
+
+app.listen(port, () => console.log('serve at local host 3000 '));
+
+
+
+
+
