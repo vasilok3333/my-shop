082399f78e549fbce2de0f2aa76e266ba75e56ac
Vasilok3333 <vasylshchur3333@gmail.com>
2021-04-29 00:38:11 +0300

    updated redux filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,31 +1,41 @@
 import React, { Component } from "react";
 import style from "./Filter.module.css";
 
-
 export default class Filter extends Component {
   render() {
     return (
       <div className={style.filter}>
-       <div className={style.filterResult}>{this.props.count} products</div>
-       <div className={style.filterSort}>Order{" "}
-       <select value={this.props.sort} onChange={this.props.sortProducts}>
-           <option>Latest</option>
-           <option value="lowest">lowest</option>
-           <option value="highest">highest</option>
-
-       </select>
-       </div>
-       <div className={style.filterCompany}>Filter{" "}
-       <select value={this.props.company} onChange={this.props.filterCompanyProducts}>
-           <option value="all">All</option>
-           <option value="samsung">Samsung</option>
-           <option value="nokia">Nokia</option>
-           <option value="htc">Htc</option>
-           <option value="apple">Iphone</option>
-           <option value="huawei">Huawei</option>
-           <option value="xiaomi">Xiaomi</option>
-       </select>
-       </div>
+        <div className={style.filterResult}>
+          {this.props.filteredProducts && this.props.filteredProducts.length > 0
+            ? this.props.filteredProducts.length + " products"
+            : null}{" "}
+        </div>
+        <div className={style.filterSort}>
+          Order{" "}
+          <select
+            value={this.props.sort}
+            onChange={(e) => this.props.sortProducts(e.target.value)}
+          >
+            <option value="latest">Latest</option>
+            <option value="lowest">lowest</option>
+            <option value="highest">highest</option>
+          </select>
+        </div>
+        <div className={style.filterCompany}>
+          Filter{" "}
+          <select
+            value={this.props.company}
+            onChange={(e) => this.props.filterProducts(e.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="samsung">Samsung</option>
+            <option value="nokia">Nokia</option>
+            <option value="htc">Htc</option>
+            <option value="apple">Iphone</option>
+            <option value="huawei">Huawei</option>
+            <option value="xiaomi">Xiaomi</option>
+          </select>
+        </div>
       </div>
     );
   }
