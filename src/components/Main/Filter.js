339fa54736cc16394eb2b5a9c4065d3a9495767e
vasilok3333 <vasylshchur3333@gmail.com>
2021-05-12 17:24:05 +0300

    deleted filter on company

--- src/components/Main/Filter.js
+++ src/components/Main/Filter.js
@@ -5,37 +5,19 @@ export default class Filter extends Component {
   render() {
     return (
       <div className={s.filter}>
-        <div className={s.filterResult}>
-          {this.props.filteredProducts && this.props.filteredProducts.length > 0
-            ? this.props.filteredProducts.length + " products"
-            : null}{" "}
-        </div>
+      
         <div className={s.filterSort}>
-          Order{" "}
+          Сортвати за цінами{" "}
           <select
             value={this.props.sort}
             onChange={(e) => this.props.sortProducts(e.target.value)}
           >
-            <option value="latest">Latest</option>
-            <option value="lowest">lowest</option>
-            <option value="highest">highest</option>
+            <option value="latest">вибірково</option>
+            <option value="lowest">дешевші</option>
+            <option value="highest">дорожчі</option>
           </select>
         </div>
-        <div className={s.filterCompany}>
-          Filter{" "}
-          <select
-            value={this.props.company}
-            onChange={(e) => this.props.filterProducts(e.target.value)}
-          >
-            <option value="all">All</option>
-            <option value="samsung">Samsung</option>
-            <option value="nokia">Nokia</option>
-            <option value="htc">Htc</option>
-            <option value="apple">Iphone</option>
-            <option value="huawei">Huawei</option>
-            <option value="xiaomi">Xiaomi</option>
-          </select>
-        </div>
+       
       </div>
     );
   }
